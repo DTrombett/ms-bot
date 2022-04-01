7ec22b784248a9094db48febf654d5b8a846b041
DTrombett <maxtromb.dt@gmail.com>
2022-04-01 20:15:23 +0200

    feat: add replit support

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { config } from "dotenv";
+import express from "express";
 import process, { env } from "node:process";
 import Constants, { CustomClient } from "./util";
 
@@ -12,10 +13,20 @@ await CustomClient.logToFile("\n");
 console.time(Constants.clientOnlineLabel);
 
 const client = new CustomClient();
+const app = express();
 (
 	global as typeof globalThis & {
-		client: CustomClient;
+		client: typeof client;
 	}
 ).client = client;
+(
+	global as typeof globalThis & {
+		app: typeof app;
+	}
+).app = app;
 
+app.use((_, res) => {
+	res.sendStatus(204);
+});
+app.listen(3000);
 await client.login();
