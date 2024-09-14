9dc3213aa07e5add3a976971dd9dc555da5c1a5d
DTrombett <d@trombett.org>
2024-09-14 16:52:53 +0200

    chore: update deps

--- src/util/verifyDiscordRequest.ts
+++ src/util/verifyDiscordRequest.ts
@@ -2,6 +2,16 @@ import { APIInteraction } from "discord-api-types/v10";
 import nacl from "tweetnacl";
 import type { Env } from ".";
 
+const hexToUint8Array = (hex: string) => {
+	const length = hex.length / 2;
+	const array = new Uint8Array(length);
+
+	for (let i = 0; i < length; i++)
+		array[i] = parseInt(hex.slice(i * 2, i * 2 + 2), 16);
+	return array;
+};
+let PUBLIC_KEY: Uint8Array | undefined;
+
 export const verifyDiscordRequest = async (request: Request, env: Env) => {
 	const signature = request.headers.get("x-signature-ed25519");
 	const timestamp = request.headers.get("x-signature-timestamp");
@@ -10,9 +20,9 @@ export const verifyDiscordRequest = async (request: Request, env: Env) => {
 		signature &&
 		timestamp &&
 		nacl.sign.detached.verify(
-			Buffer.from(timestamp + body),
-			Buffer.from(signature, "hex"),
-			Buffer.from(env.DISCORD_PUBLIC_KEY, "hex"),
+			new TextEncoder().encode(timestamp + body),
+			hexToUint8Array(signature),
+			(PUBLIC_KEY ??= hexToUint8Array(env.DISCORD_PUBLIC_KEY)),
 		);
 
 	if (!isValidRequest)
