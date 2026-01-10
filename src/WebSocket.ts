318fdec3766118d6f7c6fa316cd820cc80c2aaec
DTrombett <d@trombett.org>
2026-01-10 13:19:56 +0100

    feat: implement websocket

--- src/WebSocket.ts
+++ src/WebSocket.ts
@@ -2,6 +2,7 @@ import {
 	CompressionMethod,
 	WebSocketManager,
 	WebSocketShardEvents,
+	type SessionInfo,
 } from "@discordjs/ws";
 import {
 	WorkflowEntrypoint,
@@ -22,18 +23,23 @@ import { rest } from "./util/rest.ts";
 export type Params = {};
 
 export class WebSocket extends WorkflowEntrypoint<Env, Params> {
+	private readonly INSTANCE_KEY = "wsInstance";
 	override async run(
-		_event: Readonly<WorkflowEvent<Params>>,
+		event: Readonly<WorkflowEvent<Params>>,
 		step: WorkflowStep,
 	) {
 		await step.do(
-			"start websocket",
+			"Update KV with new instance id",
+			this.closeOldWS.bind(this, event.instanceId),
+		);
+		await step.do(
+			"Start websocket",
 			{
-				retries: { limit: 3, delay: 5_000, backoff: "exponential" },
+				retries: { limit: 15, delay: 5_000, backoff: "linear" },
 				timeout: "1 day",
 			},
 			async () => {
-				const ws = new WebSocketManager({
+				const webSocketManager = new WebSocketManager({
 					compression: CompressionMethod.ZlibNative,
 					handshakeTimeout: 5000,
 					helloTimeout: 5000,
@@ -42,7 +48,17 @@ export class WebSocket extends WorkflowEntrypoint<Env, Params> {
 					largeThreshold: 50,
 					readyTimeout: 5000,
 					rest,
+					shardCount: 1,
 					token: this.env.DISCORD_TOKEN,
+					updateSessionInfo: (shardId, sessionInfo) =>
+						sessionInfo
+							? this.env.KV.put(
+									`session&${shardId}`,
+									JSON.stringify(sessionInfo),
+							  )
+							: this.env.KV.delete(`session&${shardId}`),
+					retrieveSessionInfo: (shardId) =>
+						this.env.KV.get<SessionInfo>(`session&${shardId}`, "json"),
 				})
 					.on(WebSocketShardEvents.Dispatch, async (payload) => {
 						if (
@@ -72,7 +88,7 @@ export class WebSocket extends WorkflowEntrypoint<Env, Params> {
 								payload.d.edited_timestamp ?? payload.d.timestamp,
 							),
 						};
-						await Promise.allSettled([
+						const results = await Promise.allSettled([
 							rest.post(Routes.channelMessages(payload.d.channel_id), {
 								body: {
 									content: `**${match[1]}** ha appena iniziato il Wordle (\`${wordleStatus.startTimestamp}\`)!`,
@@ -84,8 +100,9 @@ export class WebSocket extends WorkflowEntrypoint<Env, Params> {
 								`wordle&${match[1]}`,
 							),
 						]);
+						for (const result of results)
+							if (result.status === "rejected") console.error(result.reason);
 					})
-					.on(WebSocketShardEvents.Debug, (message) => console.debug(message))
 					.on(WebSocketShardEvents.Error, (error) => console.error(error))
 					.on(WebSocketShardEvents.SocketError, (error) =>
 						console.error(error),
@@ -93,15 +110,30 @@ export class WebSocket extends WorkflowEntrypoint<Env, Params> {
 
 				try {
 					console.log("Connecting websocket");
-					await ws.connect();
+					await webSocketManager.connect();
 					console.log("Manager connected!");
-					await once(ws, WebSocketShardEvents.Closed);
-					console.log("Manager closed");
+					await once(webSocketManager, WebSocketShardEvents.Closed);
 				} finally {
-					ws.destroy();
+					webSocketManager.destroy();
+					console.log("Manager closed");
 				}
 			},
 		);
+		await step.do(
+			"Remove instance id from KV",
+			this.env.KV.delete.bind(this.env.KV, this.INSTANCE_KEY),
+		);
+	}
+
+	async closeOldWS(value: string) {
+		const id = await this.env.KV.get(this.INSTANCE_KEY);
+
+		if (id) {
+			const workflowInstance = await this.env.WEBSOCKET.get(id).catch(() => {});
+
+			await workflowInstance?.terminate().catch(() => {});
+		}
+		return this.env.KV.put(this.INSTANCE_KEY, value);
 	}
 
 	getTodayTimestamp() {
