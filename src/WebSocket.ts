07b9dcb03ad930d4b345417d304565b1a45e25e2
DTrombett <d@trombett.org>
2026-01-12 13:57:16 +0100

    refactor: simplify ws

--- src/WebSocket.ts
+++ src/WebSocket.ts
@@ -1,9 +1,4 @@
-import {
-	CompressionMethod,
-	WebSocketManager,
-	WebSocketShardEvents,
-	type SessionInfo,
-} from "@discordjs/ws";
+import { WebSocketManager, WebSocketShardEvents } from "@discordjs/ws";
 import {
 	WorkflowEntrypoint,
 	type WorkflowEvent,
@@ -13,11 +8,10 @@ import {
 	GatewayDispatchEvents,
 	GatewayIntentBits,
 	Routes,
-	type GatewayDispatchPayload,
 	type RESTPostAPIChannelMessageJSONBody,
 	type RESTPostAPIChannelMessageResult,
 } from "discord-api-types/v10";
-import { on } from "node:events";
+import { once } from "node:events";
 import { Temporal } from "temporal-polyfill";
 import { rest } from "./util/rest.ts";
 import { template } from "./util/strings.ts";
@@ -42,8 +36,8 @@ export class WebSocket extends WorkflowEntrypoint<Env, Params> {
 				timeout: "1 day",
 			},
 			async () => {
+				let ping: number | undefined, messageId: string;
 				await using emitter = new WebSocketManager({
-					compression: CompressionMethod.ZlibNative,
 					handshakeTimeout: 5_000,
 					helloTimeout: 5_000,
 					intents:
@@ -53,118 +47,114 @@ export class WebSocket extends WorkflowEntrypoint<Env, Params> {
 					rest,
 					shardCount: 1,
 					token: this.env.DISCORD_TOKEN,
-					updateSessionInfo: (shardId, sessionInfo) =>
-						sessionInfo ?
-							this.env.KV.put(`session&${shardId}`, JSON.stringify(sessionInfo))
-						:	this.env.KV.delete(`session&${shardId}`),
-					retrieveSessionInfo: (shardId) =>
-						this.env.KV.get<SessionInfo>(`session&${shardId}`, "json"),
 				})
-					.on(WebSocketShardEvents.Error, console.error)
-					.on(WebSocketShardEvents.SocketError, console.error);
-				console.log("Connecting to the WebSocket...");
-				await emitter.connect();
-				console.log("Manager connected!");
-				const id = await step.do("Send status message", () =>
-					rest
-						.post(Routes.channelMessages(this.env.STATUS_CHANNEL), {
-							body: {
-								content: "## 🟢 Status: Ready!",
-							} satisfies RESTPostAPIChannelMessageJSONBody,
-						})
-						.then((message) => (message as RESTPostAPIChannelMessageResult).id),
-				);
-				let ping: number | undefined;
+					.on(WebSocketShardEvents.SocketError, console.error)
+					.on(WebSocketShardEvents.HeartbeatComplete, (stats) =>
+						rest.patch(
+							Routes.channelMessage(this.env.STATUS_CHANNEL, messageId),
+							{
+								body: {
+									content: template`
+										## 🟢 Status: Ready!
+										Latency: **${(ping = stats.latency)}ms**
+										Last heartbeat: <t:${Math.round(stats.heartbeatAt / 1000)}:T> (<t:${Math.round(stats.heartbeatAt / 1000)}:R>)
+									`,
+								} satisfies RESTPostAPIChannelMessageJSONBody,
+							},
+						),
+					)
+					.on(WebSocketShardEvents.Dispatch, async (payload) => {
+						try {
+							if (
+								(payload.t !== GatewayDispatchEvents.MessageCreate &&
+									payload.t !== GatewayDispatchEvents.MessageUpdate) ||
+								payload.d.author.id === this.env.DISCORD_APPLICATION_ID
+							)
+								return;
+							const {
+								d,
+								d: { author },
+							} = payload;
 
-				emitter.on(WebSocketShardEvents.HeartbeatComplete, (stats) => {
-					ping = stats.latency;
-					return rest.patch(
-						Routes.channelMessage(this.env.STATUS_CHANNEL, id),
-						{
-							body: {
-								content: template`
-									## 🟢 Status: Ready!
-									Latency: **${stats.latency}ms**
-									Last heartbeat: <t:${Math.round(stats.heartbeatAt / 1_000)}:T> (<t:${Math.round(stats.heartbeatAt / 1_000)}:R>)
-								`,
-							} satisfies RESTPostAPIChannelMessageJSONBody,
-						},
-					);
-				});
-				for await (const [payload] of on(
-					emitter,
-					WebSocketShardEvents.Dispatch,
-					{ close: [WebSocketShardEvents.Closed] },
-				) as NodeJS.AsyncIterator<[payload: GatewayDispatchPayload]>)
-					try {
-						if (
-							(payload.t !== GatewayDispatchEvents.MessageCreate &&
-								payload.t !== GatewayDispatchEvents.MessageUpdate) ||
-							payload.d.author.id === this.env.DISCORD_APPLICATION_ID
-						)
-							continue;
-						const {
-							d,
-							d: { author },
-						} = payload;
+							if (
+								d.content.startsWith(`<@${this.env.DISCORD_APPLICATION_ID}>`)
+							) {
+								let [, command] = d.content.trimStart().split(/\s+/g);
 
-						if (d.content.startsWith(`<@${this.env.DISCORD_APPLICATION_ID}>`)) {
-							let [, command] = d.content.trimStart().split(/\s+/g);
-
-							command = command?.toLowerCase();
-							if (command === "ping")
-								await rest.post(Routes.channelMessages(d.channel_id), {
+								command = command?.toLowerCase();
+								if (command === "ping")
+									await rest.post(Routes.channelMessages(d.channel_id), {
+										body: {
+											content: template`
+											### 🏓 Pong!
+											${ping}WS: \`${ping}ms\`
+											`,
+											message_reference: {
+												message_id: d.id,
+												fail_if_not_exists: false,
+											},
+											allowed_mentions: { parse: [] },
+										} satisfies RESTPostAPIChannelMessageJSONBody,
+									});
+								return;
+							}
+							if (
+								d.channel_id !== this.env.WORDLE_CHANNEL ||
+								author.id !== "1211781489931452447"
+							)
+								return;
+							const match = d.content.match(/^(.+) (?:is|and .+ are) playing/);
+							if (!match?.[1]) return;
+							let wordleStatus = await this.env.KV.get<{
+								startTimestamp?: number;
+								words?: number /* Infinity for X */;
+								endTimestamp?: number;
+							}>(`wordle&${match[1]}`, "json");
+							if (
+								wordleStatus?.startTimestamp &&
+								wordleStatus.startTimestamp >= this.getTodayTimestamp()
+							)
+								return;
+							wordleStatus = {
+								startTimestamp: Date.parse(d.edited_timestamp ?? d.timestamp),
+							};
+							const results = await Promise.allSettled([
+								rest.post(Routes.channelMessages(d.channel_id), {
 									body: {
-										content: template`
-										### 🏓 Pong!
-										${ping}WS: \`${ping}ms\`
-										`,
-										message_reference: {
-											message_id: d.id,
-											fail_if_not_exists: false,
-										},
+										content: `**${match[1]}** ha appena iniziato il Wordle (\`${wordleStatus.startTimestamp}\`)!`,
 										allowed_mentions: { parse: [] },
 									} satisfies RESTPostAPIChannelMessageJSONBody,
-								});
-							continue;
+								}),
+								this.env.KV.put(
+									`wordle&${match[1]}`,
+									JSON.stringify(wordleStatus),
+								),
+							]);
+							for (const result of results)
+								if (result.status === "rejected") console.error(result.reason);
+						} catch (err) {
+							console.error(err);
 						}
-						if (
-							d.channel_id !== this.env.WORDLE_CHANNEL ||
-							author.id !== "1211781489931452447"
-						)
-							continue;
-						const match = d.content.match(/^(.+) (?:is|and .+ are) playing/);
-						if (!match?.[1]) continue;
-						let wordleStatus = await this.env.KV.get<{
-							startTimestamp?: number;
-							words?: number /* Infinity for X */;
-							endTimestamp?: number;
-						}>(`wordle&${match[1]}`, "json");
-						if (
-							wordleStatus?.startTimestamp &&
-							wordleStatus.startTimestamp >= this.getTodayTimestamp()
-						)
-							continue;
-						wordleStatus = {
-							startTimestamp: Date.parse(d.edited_timestamp ?? d.timestamp),
-						};
-						const results = await Promise.allSettled([
-							rest.post(Routes.channelMessages(d.channel_id), {
+					});
+
+				console.log("Connecting to the WebSocket...");
+				await emitter.connect();
+				step
+					.do("Send status message", () =>
+						rest
+							.post(Routes.channelMessages(this.env.STATUS_CHANNEL), {
 								body: {
-									content: `**${match[1]}** ha appena iniziato il Wordle (\`${wordleStatus.startTimestamp}\`)!`,
-									allowed_mentions: { parse: [] },
+									content: "## 🟢 Status: Ready!",
 								} satisfies RESTPostAPIChannelMessageJSONBody,
-							}),
-							this.env.KV.put(
-								`wordle&${match[1]}`,
-								JSON.stringify(wordleStatus),
+							})
+							.then(
+								(message) => (message as RESTPostAPIChannelMessageResult).id,
 							),
-						]);
-						for (const result of results)
-							if (result.status === "rejected") console.error(result.reason);
-					} catch (err) {
-						console.error(err);
-					}
+					)
+					.then((id) => (messageId = id))
+					.catch(console.error);
+				console.log("Manager connected!");
+				await once(emitter, WebSocketShardEvents.Closed);
 			},
 		);
 		await step.do(
