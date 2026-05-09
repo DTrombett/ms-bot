52d2c7ee3e7f1acc1da8272fc1ba2d05782b4444
DTrombett <d@trombett.org>
2026-05-09 12:22:37 +0200

    fix: add guildId tournament option

--- src/app/components/Tournament.tsx
+++ src/app/components/Tournament.tsx
@@ -1,3 +1,4 @@
+import { env } from "cloudflare:workers";
 import { Suspense } from "react";
 import { Temporal } from "temporal-polyfill";
 import {
@@ -33,7 +34,7 @@ export default ({
 
 	return (
 		<form
-			method={"POST"}
+			method="POST"
 			action={
 				tournament ? `/tournaments/${tournament.id}/edit` : "/tournaments"
 			}
@@ -60,6 +61,15 @@ export default ({
 					placeholder="Il nome del torneo"
 					required
 					defaultValue={tournament?.name}
+				/>
+				<TextInput
+					name="guildId"
+					label="ID server torneo"
+					placeholder="L'ID del server dove si svolge il torneo"
+					pattern="\d{16,32}"
+					errorMessage="ID non valido"
+					required
+					defaultValue={tournament?.guildId ?? env.MAIN_GUILD}
 				/>
 				<TextInput
 					name="logChannel"
